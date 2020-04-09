d6ecac5821a464f5064e5232b988a693d328e7ec
Michael Stevens <stevensmike36@yahoo.com>
2020-04-09 13:52:32 -0500

    added player, room classes, cmd parser started

--- src/player.py
+++ src/player.py
@@ -1,6 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
 class Player:
     def __init__(self, player_name, current_room):
         self.player_name = player_name
         self.current_room = current_room 
+        
+        def move(self, room):
+            self.room = room[room_name]
--- src/room.py
+++ src/room.py
@@ -4,7 +4,8 @@ class Room:
     def __init__(self, room_name, room_descrp, n_to=None, s_to=None, e_to=None, w_to=None):
         self.room_name = room_name
         self.room_descrp = room_descrp
-        self.n_to = n_to
-        self.s_to = s_to
-        self.e_to = e_to
-        self.w_to = w_to
+        
+        self.n_to = [] if n_to is None else n_to
+        self.s_to = [] if s_to is None else s_to
+        self.e_to = [] if e_to is None else e_to
+        self.w_to = [] if w_to is None else w_to
